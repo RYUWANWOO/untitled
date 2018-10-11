e33ef9f1a54734aa2bb34a3c4bdcdbdf9d099246
RYUWANWOO <reo0527@naver.com>
2018-10-11 16:19:06 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout<<"hello"<<std::endl;
+
+    int a(10);
+
+    cin>>a;
+
+    cout<<a;
+
+
+    std::cout<<"hello";
+
+    return 0;
+}
